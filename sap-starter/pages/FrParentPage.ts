cf5d22cdf5899f79eaf83ef911d2c9e4a98e7abe
andreea borzasi <andreeaborzasi@gmail.com>
2026-06-26 09:34:56 +0300

    added filterInput

--- sap-starter/pages/FrParentPage.ts
+++ sap-starter/pages/FrParentPage.ts
@@ -1,9 +1,19 @@
-import type { Page } from "@playwright/test";
+import type { Locator, Page } from "@playwright/test";
 
 export class FrParentPage {
   readonly page: Page;
+  readonly filterESID: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    this.filterESID = this.filterInput('ESID');
+    this.filterFRGUID= this.filterInput('FR_GUID');
+    this.filterESID = this.filterInput('ESID');
+  }
+
+
+  //todo
+  filterInput(key:string):Locator{
+    return this.page.getByPlaceholder(new RegExp(`Filter by \\s+${key}`,"i"));
   }
 }
